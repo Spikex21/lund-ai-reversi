67901353a4180871b421f9e8c36ae3290a98ad4c
bwalton9 <benwalton@bens-mbp>
2018-02-03 18:50:02 +0100

    I finished the minimax with alpha-beta parsing for the AI class. I also added timing in the Reversi class to show the time each move takes and overall game time. I also fixed when the game finishes. It should end when both players have no valid moves. If one player cannot move, then that players turn is skipped. Having no available moves is taken into account for the AI class.

--- ReversiAi/src/player/Ai.java
+++ ReversiAi/src/player/Ai.java
@@ -7,7 +7,7 @@ import game.Board;
 
 public class Ai extends Player{
 	
-	public static final int TREE_DEPTH = 3;
+	public static final int TREE_DEPTH = 9;
 	private char opponentPiece;
 
 	public Ai(String name, char teamPiece) {
@@ -19,6 +19,26 @@ public class Ai extends Player{
 		opponentPiece = (Board.BLACK_TILE == teamPiece)? Board.WHITE_TILE : Board.BLACK_TILE; 
 		Decision bestDecision = null;
 		int bestChoice = Integer.MIN_VALUE;
+		
+		for (int x = 1; x <= 8; x++) {
+			for (int y = 1; y <= 8; y++) {
+				Decision tempDecision = new Decision(x, y, teamPiece);
+				
+				if (board.isValidMove(tempDecision)) {
+					Board tempBoard = new Board(board);
+					
+					tempBoard.board[tempDecision.x - 1][tempDecision.y - 1] = teamPiece;
+					
+					int value = minValue(tempBoard, bestChoice, Integer.MAX_VALUE, TREE_DEPTH);
+					
+					if (value > bestChoice) {
+						bestChoice = value;
+						bestDecision = tempDecision;
+					}
+				}
+			}
+		}
+		
 		
 		//TODO 	for (all spots on the board...)
 		//			if(it is not a valid move)
@@ -34,24 +54,77 @@ public class Ai extends Player{
 		
 		
 		
-		return null;
+		return bestDecision;
 	}
 	
 	private int maxValue(Board board, int alpha, int beta, int depth) {
 		depth--;
-		if(depth == 0)
+		if (depth == 0 || !board.areValidMoves(teamPiece)) {
 			return board.scoreBoard(teamPiece);
-		/*
-		 * from video stuff
-		 */
-		return 0;
+		}
+		
+		int maxVal = Integer.MIN_VALUE;
+	
+		for (int x = 1; x <= 8; x++) {
+			for (int y = 1; y <= 8; y++) {
+				Decision tempDecision = new Decision(x, y, teamPiece);
+				
+				if (board.isValidMove(tempDecision)) {
+					Board tempBoard = new Board(board);
+					tempBoard.board[tempDecision.x - 1][tempDecision.y - 1] = teamPiece;
+
+					int val = minValue(tempBoard, alpha, beta, depth);
+					
+					if (val > maxVal) {
+						maxVal = val;
+					}
+					
+					if (val >= beta) {
+						return maxVal;
+					}
+						
+					if (val > alpha) {
+						alpha = val;
+					}
+				}
+			}
+		}
+
+		return maxVal;
 	}
 	
 	private int minValue(Board board, int alpha, int beta, int depth) {
-		/*
-		 * from video stuff
-		 */
-		return 0;
+		depth--;
+		if (depth == 0 || !board.areValidMoves(opponentPiece)) {
+			return board.scoreBoard(teamPiece);
+		}
+		
+		int minVal = Integer.MAX_VALUE;
+	
+		for (int x = 1; x <= 8; x++) {
+			for (int y = 1; y <= 8; y++) {
+				Decision tempDecision = new Decision(x, y, opponentPiece);
+				
+				if (board.isValidMove(tempDecision)) {
+					Board tempBoard = new Board(board);
+					tempBoard.board[tempDecision.x - 1][tempDecision.y - 1] = opponentPiece;
+		
+					int val = maxValue(tempBoard, alpha, beta, depth);
+					
+					if (val < minVal) {
+						minVal = val;
+					}
+					
+					if (val <= alpha) {
+						return minVal;
+					}
+						
+					if (val < beta) {
+						beta = val;
+					}
+				}
+			}
+		}
+		return minVal;
 	}
-
 }

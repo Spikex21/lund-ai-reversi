302317395ac0c84b34ae2eae458ca5100d0cf817
Evan <Evan@192.168.1.104>
2018-02-03 15:27:21 +0100

    added pseudocode for AI implementation

--- ReversiAi/src/player/Ai.java
+++ ReversiAi/src/player/Ai.java
@@ -6,6 +6,9 @@ import decision.Decision;
 import game.Board;
 
 public class Ai extends Player{
+	
+	public static final int TREE_DEPTH = 3;
+	private char opponentPiece;
 
 	public Ai(String name, char teamPiece) {
 		super(name, teamPiece);
@@ -13,15 +16,42 @@ public class Ai extends Player{
 
 	@Override
 	public Decision getDecision(Board board) {
-		//TODO temporary random choice AI
-		Decision d = null;
-		Random rand = new Random();
-		do {
-			int x = rand.nextInt(8)+1;
-			int y = rand.nextInt(8)+1;
-			d = new Decision(x, y, teamPiece);
-		}while(!board.isValidMove(d));
-		return d;
+		opponentPiece = (Board.BLACK_TILE == teamPiece)? Board.WHITE_TILE : Board.BLACK_TILE; 
+		Decision bestDecision = null;
+		int bestChoice = Integer.MIN_VALUE;
+		
+		//TODO 	for (all spots on the board...)
+		//			if(it is not a valid move)
+		//				continue;
+		
+		//			Decision tempDecision = whatever the valid move was;
+		//			Board tempBoard = new Board(board);
+		//			tempBoard.makeDecision(tempDecision);
+		//			int value = minValue(tempBoard, bestChoice, Integer.Max_VALUE, TREE_DEPTH);
+		//			if(value > bestChoice)
+		//				bestDecision = tempDecision;
+		//					
+		
+		
+		
+		return null;
+	}
+	
+	private int maxValue(Board board, int alpha, int beta, int depth) {
+		depth--;
+		if(depth == 0)
+			return board.scoreBoard(teamPiece);
+		/*
+		 * from video stuff
+		 */
+		return 0;
+	}
+	
+	private int minValue(Board board, int alpha, int beta, int depth) {
+		/*
+		 * from video stuff
+		 */
+		return 0;
 	}
 
 }
